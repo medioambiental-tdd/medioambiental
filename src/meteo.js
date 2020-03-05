dc471a8366b7bcd924d35e28543ee12a6f2dc9df
Victor Peralta <victorperalta.correo@gmail.com>
2020-03-05 15:47:29 +0100

    añadida funcion para prediccion meteorologica. Avanza en #9 y #3

---
+++ src/meteo.js
@@ -0,0 +1,17 @@
+function prediccion_meteo(municipio, dia){
+    var json = {
+        "municipio" : "Granada",
+        "estado_cielo": "nublado",
+        "prob_precipitacion" : "13",
+        "prob_nieve" : "0",
+        "temperatura" : "17",
+        "sensacion_termica" : "15",
+        "velocidad_viento":"5",
+        "direccion_viento":"WE",
+        "amanecer":"7",
+        "ocaso":"20"
+    };
+
+    return json;
+}
+
