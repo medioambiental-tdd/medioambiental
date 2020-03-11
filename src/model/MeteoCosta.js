dd5ae6304395c6315abe4fa04ec2d634f74a6e92
javilecrin <javilecrin@correo.ugr.es>
2020-03-11 14:58:26 +0100

    Avanzo en HU3 ref #5

--- src/model/MeteoCosta.js
+++ src/model/MeteoCosta.js
@@ -1,3 +1,48 @@
 class MeteoCosta{
-    constructor(estado_cielo, viento, oleaje, temp_agua);
+    constructor(){
+        this.nombrePlaya;
+        this.codigo;
+        this.estado_cielo=null;
+        this.viento=null;
+        this.oleaje=null;
+        this.temp_agua=null;
+        this.provincia=null;
+        this.playa=null;
+    }
+
+    ComprobarExistePlaya(NombrePlaya){
+        
+    }
+
+    PedirDatosPlaya(nombrePlaya){
+        
+    }
+
+    getEstadoCielo(){
+
+    }
+
+    getViento(){
+
+    }
+
+    getOleaje(){
+
+    }
+
+    getTempAgua(){
+
+    }
+
+    getPlaya(){
+
+    }
+
+    getProvincia(){
+
+    }
+
+    getPlaya(){
+
+    }
 }
---
+++ src/model/Playa.js
@@ -0,0 +1,19 @@
+class Playa{
+    Constructor(nombre,codigo){
+       this.nombre=nombre;
+       this.codigo=codigo;
+    }
+ 
+    getNombre(){
+       return nombre;
+    }
+ 
+    getCodigo(){
+       return codigo;
+    }
+ 
+ }
+ 
+ function extraerPlayas(){
+    
+ }
