26623506254f7b1382a783964a426bf6e364e87f
Victor Peralta <victorperalta.correo@gmail.com>
2020-03-11 14:49:50 +0100

    metodo para pedir datos implementado. Avanza en #4

--- src/model/MeteoTextual.js
+++ src/model/MeteoTextual.js
@@ -1,3 +1,18 @@
+const predicciones  = require('../services/predicciones');
+
 class MeteoTextual{
-    constructor(zona, info);
+    constructor(zona, dia){
+        this.zona = zona;
+        this.dia = dia;
+
+        this.pedirDatosTextual();
+    }
+
+    pedirDatosTextual(){
+        (async () => {
+            this.texto = await predicciones(this.zona,this.dia);
+        })()
+    }
 }
+
+mt = new MeteoTextual("nacional","hoy");
