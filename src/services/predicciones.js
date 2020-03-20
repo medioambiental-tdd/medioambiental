0e682ef4742ebd88473d72034fbb8d5f05eded2b
Victor Peralta <victorperalta.correo@gmail.com>
2020-03-20 22:39:36 +0100

    corrige sangrado del proyecto

--- src/services/predicciones.js
+++ src/services/predicciones.js
@@ -11,48 +11,54 @@ async function get_prediccion_municipio(municipio,get_datos){
     var json= await datos.json();
     var nombre=json[0].nombre;
     var fecha=json[0].elaborado.slice(0,10);
-    var probPrecipitacion=[json[0].prediccion.dia[0].probPrecipitacion[3].value,
-                           json[0].prediccion.dia[0].probPrecipitacion[4].value,
-                           json[0].prediccion.dia[0].probPrecipitacion[5].value,
-                           json[0].prediccion.dia[0].probPrecipitacion[6].value,
-                          ];
-    var cotaNieve=[json[0].prediccion.dia[0].cotaNieveProv[3].value,
-                   json[0].prediccion.dia[0].cotaNieveProv[4].value,
-                   json[0].prediccion.dia[0].cotaNieveProv[5].value,
-                   json[0].prediccion.dia[0].cotaNieveProv[6].value
-                  ];
+    var probPrecipitacion=[
+        json[0].prediccion.dia[0].probPrecipitacion[3].value,
+        json[0].prediccion.dia[0].probPrecipitacion[4].value,
+        json[0].prediccion.dia[0].probPrecipitacion[5].value,
+        json[0].prediccion.dia[0].probPrecipitacion[6].value,
+    ];
+    
+    var cotaNieve=[
+        json[0].prediccion.dia[0].cotaNieveProv[3].value,
+        json[0].prediccion.dia[0].cotaNieveProv[4].value,
+        json[0].prediccion.dia[0].cotaNieveProv[5].value,
+        json[0].prediccion.dia[0].cotaNieveProv[6].value
+    ];
 
-    var estadoCielo=[json[0].prediccion.dia[0].estadoCielo[3].descripcion,
-                     json[0].prediccion.dia[0].estadoCielo[4].descripcion,
-                     json[0].prediccion.dia[0].estadoCielo[5].descripcion,
-                     json[0].prediccion.dia[0].estadoCielo[6].descripcion
-                    ];
+    var estadoCielo=[
+        json[0].prediccion.dia[0].estadoCielo[3].descripcion,
+        json[0].prediccion.dia[0].estadoCielo[4].descripcion,
+        json[0].prediccion.dia[0].estadoCielo[5].descripcion,
+        json[0].prediccion.dia[0].estadoCielo[6].descripcion
+    ];
 
-    var temperatura=[json[0].prediccion.dia[0].temperatura.dato[0].value,
-                     json[0].prediccion.dia[0].temperatura.dato[1].value,
-                     json[0].prediccion.dia[0].temperatura.dato[2].value,
-                     json[0].prediccion.dia[0].temperatura.dato[3].value
-                    ];
-
+    var temperatura=[
+        json[0].prediccion.dia[0].temperatura.dato[0].value,
+        json[0].prediccion.dia[0].temperatura.dato[1].value,
+        json[0].prediccion.dia[0].temperatura.dato[2].value,
+        json[0].prediccion.dia[0].temperatura.dato[3].value
+    ];
                 
-     var sensTermica=[json[0].prediccion.dia[0].sensTermica.dato[0].value,
-                      json[0].prediccion.dia[0].sensTermica.dato[1].value,
-                      json[0].prediccion.dia[0].sensTermica.dato[2].value,
-                      json[0].prediccion.dia[0].sensTermica.dato[3].value
-                   ];
+    var sensTermica=[
+        json[0].prediccion.dia[0].sensTermica.dato[0].value,
+        json[0].prediccion.dia[0].sensTermica.dato[1].value,
+        json[0].prediccion.dia[0].sensTermica.dato[2].value,
+        json[0].prediccion.dia[0].sensTermica.dato[3].value
+    ];
 
+    var velocidadViento=[
+        json[0].prediccion.dia[0].viento[3].velocidad,
+        json[0].prediccion.dia[0].viento[4].velocidad,
+        json[0].prediccion.dia[0].viento[5].velocidad,
+        json[0].prediccion.dia[0].viento[6].velocidad
+    ];
 
-    var velocidadViento=[json[0].prediccion.dia[0].viento[3].velocidad,
-                         json[0].prediccion.dia[0].viento[4].velocidad,
-                         json[0].prediccion.dia[0].viento[5].velocidad,
-                         json[0].prediccion.dia[0].viento[6].velocidad
-                        ];
-
-    var direccionViento=[json[0].prediccion.dia[0].viento[3].direccion,
-                        json[0].prediccion.dia[0].viento[4].direccion,
-                        json[0].prediccion.dia[0].viento[5].direccion,
-                        json[0].prediccion.dia[0].viento[6].direccion
-                       ];
+    var direccionViento=[
+        json[0].prediccion.dia[0].viento[3].direccion,
+        json[0].prediccion.dia[0].viento[4].direccion,
+        json[0].prediccion.dia[0].viento[5].direccion,
+        json[0].prediccion.dia[0].viento[6].direccion
+    ];
 
     mm= new MeteoMunicipio(nombre,fecha,estadoCielo,probPrecipitacion,cotaNieve,temperatura,sensTermica,velocidadViento,direccionViento);
     return mm;    
